5fdc71af4655c4850877a1998b5a23659c5fa0a3
Marcos R. Silva <w271592m@motorola.com>
2023-02-08 07:09:25 -0300

    create the layer method

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -5,6 +5,7 @@ import {Construct} from 'constructs'
 //---------------creating storage-----------------------//
 import * as dynaDB from 'aws-cdk-lib/aws-dynamodb'
 //----------------------------------------------------//
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 export class ProductsAppStack extends cdk.Stack {
     readonly productsFetchHandler: lambdaNodeJS.NodejsFunction; //it's referencing my function
@@ -27,6 +28,10 @@ export class ProductsAppStack extends cdk.Stack {
             writeCapacity: 1
         });
 
+        //ProductsLayers
+        const productsLayerArn = ssm.StringParameter.valueForStringParameter(this,"productsLayersVersionArn")
+        const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "productsLayersVersionArn", productsLayerArn)
+
         //productsFetchHandler can be used in the constructor of the archive ecommerceApi-stack when using only .js extension
         //This method was built to be able to perform action as: GET requests
         this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this,
@@ -42,7 +47,8 @@ export class ProductsAppStack extends cdk.Stack {
                 },
                 environment: {
                     PRODUCTS_DDB: this.productsDdb.tableName
-                }
+                },
+                layers: [productsLayer] //here I'm setting permissions to this method access the layers and share information
             });
         this.productsDdb.grantReadData(this.productsFetchHandler); //permission for the function access the table
 
@@ -60,7 +66,8 @@ export class ProductsAppStack extends cdk.Stack {
                 },
                 environment: {
                     PRODUCTS_DDB: this.productsDdb.tableName
-                }
+                },
+                layers: [productsLayer] //here I'm setting permissions to this method access the layers and share information
             });
         this.productsDdb.grantWriteData(this.productsAdminHandler); //permission to the function access the table
     }
