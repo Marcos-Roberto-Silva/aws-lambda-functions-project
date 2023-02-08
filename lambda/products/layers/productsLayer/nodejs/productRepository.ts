542d8426281c187b9db1f4781c0a7542105c3d74
Marcos R. Silva <w271592m@motorola.com>
2023-02-08 08:24:21 -0300

    create method to get product by its id

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -24,4 +24,19 @@ export class ProductRepository {
         }).promise()
         return data.Items as Product[]
     }
+
+    async getProductById(productId: string): Promise<Product> {
+        const data = await this.ddbClient.get({
+            TableName: this.productsDdb,
+            Key: {
+                id: productId
+            }
+        }).promise()
+
+        if (data.Item) {
+            return data.Item as Product
+        } else {
+            throw new Error(`Product with id:${productId} not found`)
+        }
+    }
 }
